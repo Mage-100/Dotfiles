a868e988807a3c61ed517f2147b0ec95c69d35c7
Azwad Jahan <azwadjahan77@gmail.com>
2025-04-14 15:07:20 +0530

    Added kitty and some config changes

--- nvim2/.config/nvim/lua/plugins/kanagawa.lua
+++ nvim2/.config/nvim/lua/plugins/kanagawa.lua
@@ -1,10 +1,10 @@
 return {
-  "rebelot/kanagawa.nvim", 
-  config=function()
+  "rebelot/kanagawa.nvim",
+  config = function()
     require("kanagawa").setup({
-      compile=true,
-      transparent=true,
-      overrides=function(colors)
+      compile = false,
+      transparent = true,
+      overrides = function(colors)
         return {
           ["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
           ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
@@ -12,12 +12,22 @@ return {
           ["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
           ["@markup.list.markdown"] = { link = "Function" }, -- + list
           ["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
-          ["@markup.list.checked.markdown"] = { link = "WarningMsg" } -- [x] checked list item
+          ["@markup.list.checked.markdown"] = { link = "WarningMsg" }, -- [x] checked list item
         }
-      end
-    });
-    vim.cmd("colorscheme kanagawa");
+      end,
+      -- colors = {
+      --   theme = {
+      --     wave = {
+      --       syn = {
+      --         comment = "#999988",
+      --       },
+      --     },
+      --   },
+      -- },
+    })
+    vim.cmd("colorscheme kanagawa")
   end,
-  build=function()
-    vim.cmd("KanagawaCompile"); end
-} 
+  build = function()
+    vim.cmd("KanagawaCompile")
+  end,
+}
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.front_end = "OpenGL"
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.8
 config.enable_tab_bar = false
