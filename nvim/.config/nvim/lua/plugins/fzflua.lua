b9f213c3e5674f63922d8b2060076b6b3e4cd3d3
Azwad Jahan <azwadjahan77@gmail.com>
2025-07-06 15:47:12 +0530

    Did some config changes

--- nvim/.config/nvim/lua/plugins/fzflua.lua
+++ nvim/.config/nvim/lua/plugins/fzflua.lua
@@ -1,52 +1,59 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "echasnovski/mini.icons" },
-  opts = {},
-  keys={ -- So that lazy will load fzf-lua on pressing the key binding
-    { 
-      "<leader>ff",
-      function() 
-        require("fzf-lua").files()
-      end,
-      desc="Find Files in working directory"
-    },
-    { 
-      "<leader>fg",
-      function() 
-        require("fzf-lua").live_grep()
-      end,
-      desc="Find by grepping working directory"
-    },
-    { 
-      "<leader>fc",
-      function() 
-        require("fzf-lua").files({ cwd=vim.fn.stdpath("config") })
-      end,
-      desc="Find in neovim configuration"
-    },
-    { 
-      "<leader>fk",
-      function() 
-        require("fzf-lua").keymaps()
-      end,
-      desc="[F]ind [K]eymaps"
-    },
-    { 
-      "<leader>fb",
-      function() 
-        require("fzf-lua").builtin()
-      end,
-      desc="[F]ind [B]uiltin FZF"
-    }, 
-    { 
-      "<leader><leader>",
-      function() 
-        require("fzf-lua").buffers()
-      end,
-      desc="Find existing buffers"
-    }
-  }
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "echasnovski/mini.icons" },
+	opts = {},
+	keys = { -- So that lazy will load fzf-lua on pressing the key binding
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find Files in working directory",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "Find by grepping working directory",
+		},
+		{
+			"<leader>fc",
+			function()
+				require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Find in neovim configuration",
+		},
+		{
+			"<leader>fk",
+			function()
+				require("fzf-lua").keymaps()
+			end,
+			desc = "[F]ind [K]eymaps",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").builtin()
+			end,
+			desc = "[F]ind [B]uiltin FZF",
+		},
+		{
+			"<leader><leader>",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Find existing buffers",
+		},
+		{
+			"<leader>gts",
+			function()
+				require("fzf-lua").git_status()
+			end,
+			desc = "Git Status",
+		},
+	},
 }
